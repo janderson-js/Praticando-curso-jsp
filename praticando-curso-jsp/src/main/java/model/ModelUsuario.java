7943e0311e129fbdc45abfae150e6eb4b498d9b0
Janderson Souza <jandersonsouzagit@gmail.com>
2021-10-15 21:32:47 -0300

    Atributos para modelUsuario e ModelTelefone

--- praticando-curso-jsp/src/main/java/model/ModelUsuario.java
+++ praticando-curso-jsp/src/main/java/model/ModelUsuario.java
@@ -1,5 +1,177 @@
 package model;
 
-public class ModelUsuario {
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
 
+public class ModelUsuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private Date dataNascimento;
+	private String cpf;
+	private String cep;
+	private String cidade;
+	private String uf;
+	private String bairro;
+	private String logradouro;
+	private String numero;
+	private String fotoUser;
+	private String extensaoFotoUser;
+	private String perfil;
+	private List<ModelTelefone> Telefones;
+	private Long idUserCad;
+	
+	private Boolean userAdmin;
+	
+	private String login;
+	private String senha;
+	
+	
+
+	public List<ModelTelefone> getTelefones() {
+		return Telefones;
+	}
+
+	public void setTelefones(List<ModelTelefone> telefones) {
+		Telefones = telefones;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getFotoUser() {
+		return fotoUser;
+	}
+
+	public void setFotoUser(String fotoUser) {
+		this.fotoUser = fotoUser;
+	}
+
+	public String getExtensaoFotoUser() {
+		return extensaoFotoUser;
+	}
+
+	public void setExtensaoFotoUser(String extensaoFotoUser) {
+		this.extensaoFotoUser = extensaoFotoUser;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+	
+	public Long getIdUserCad() {
+		return idUserCad;
+	}
+
+	public void setIdUserCad(Long idUserCad) {
+		this.idUserCad = idUserCad;
+	}
+
+	public Boolean getUserAdmin() {
+		return userAdmin;
+	}
+
+	public void setUserAdmin(Boolean userAdmin) {
+		this.userAdmin = userAdmin;
+	}
 }
