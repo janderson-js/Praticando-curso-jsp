d55adf3fbef36249bf184f9135906e841e37df28
Janderson Souza <jandersonsouzagit@gmail.com>
2021-10-16 15:41:53 -0300

    Classe de conexão com o banco de dados

---
+++ praticando-curso-jsp/src/main/java/connection/SingleConnectionBanco.java
@@ -0,0 +1,39 @@
+package connection;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnectionBanco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static Connection connection = null;
+	
+	private static String user = "postgres";
+	private static String senha = "admin";
+	private static String url = "jdbc:postgresql://localhost:5433/pratica-curso-jsp?autoreconnect=true";
+	
+	public SingleConnectionBanco() throws Exception{
+		conectar();
+	}
+	
+	public static void conectar() throws Exception {
+		try {
+			if(connection == null) {
+				
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, senha);
+				connection.setAutoCommit(false);
+			}
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+			
+			e.printStackTrace();
+		}
+	}
+}
